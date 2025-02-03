5d0297b4b93a6049009d56a1b6a24736aa1f369c
Federico Rossendy <fede070893@gmail.com>
2025-02-03 14:12:43 -0300

    Add Pagination component
    Introduce a reusable Pagination component with "Next" and "Previous" buttons. It enables navigation between pages and manages the current page state.

---
+++ src/components/Pagination.js
@@ -0,0 +1,14 @@
+const Pagination = ({ page, setPage}) => {
+  const nextPage = () => setPage(page + 1)
+  const prevPage = () => setPage(page - 1)
+
+  return(
+    <div className="pagination">
+      <button onClick={prevPage} disabled={page === 1}>Previous</button>
+      <span>{page}</span>
+      <button onClick={nextPage}>Next</button>
+    </div>
+  )
+}
+
+export default Pagination
